84412b3b1fa0cf293d36772e15ea30a0cc471b85
Couvez olivier <o.couvez@free.fr>
2021-04-15 09:40:26 +0200

    first commit

---
+++ ILecteurAudio.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfLecteurAudio
+{
+    public interface ILecteurAudio
+    {
+        bool LectureEnCours { get; }
+        bool EstEnPause { get; }
+        void Lire(string cheminFichier);
+        void Pause();
+        void ReprendreLecture();
+        void Stop();
+
+    }
+}
---
+++ LecteurMp3.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfLecteurAudio
+{
+    class LecteurMp3 : ILecteurAudio
+    {
+
+        bool ILecteurAudio.LectureEnCours => throw new NotImplementedException();
+
+        bool ILecteurAudio.EstEnPause => throw new NotImplementedException();
+
+        void ILecteurAudio.Lire(string cheminFichier)
+        {
+            throw new NotImplementedException();
+        }
+
+        void ILecteurAudio.Pause()
+        {
+            throw new NotImplementedException();
+        }
+
+        void ILecteurAudio.ReprendreLecture()
+        {
+            throw new NotImplementedException();
+        }
+
+        void ILecteurAudio.Stop()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
