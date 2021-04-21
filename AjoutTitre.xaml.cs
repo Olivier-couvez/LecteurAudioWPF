c7773784c2795172a686b27c3ef92cc53d41b4f8
Couvez olivier <o.couvez@free.fr>
2021-04-21 23:43:53 +0200

    Modificdation pour lire le tag mp3

--- AjoutTitre.xaml.cs
+++ AjoutTitre.xaml.cs
@@ -1,6 +1,8 @@
-﻿using Microsoft.Win32;
+﻿using Microsoft.Graph;
+using Microsoft.Win32;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -24,6 +26,8 @@ namespace WpfLecteurAudio
         List<Tracks> listeMorceaux;
         enregFichier sauvegarde;
         Tracks nouveauMorceau;
+        string chemin = "";
+        string[] decoupe;
         public AjoutTitre()
         {
             InitializeComponent();
@@ -37,29 +41,70 @@ namespace WpfLecteurAudio
 
         }
 
-        private void boutonAjouter_Click(object sender, RoutedEventArgs e)
+       
+        private void boutonRechercheFichier_Click(object sender, RoutedEventArgs e)
         {
-
-            string chemin = "";
             OpenFileDialog openFileDialog = new OpenFileDialog();
             openFileDialog.Filter = "Media files (*.mp3;*.mpg;*.mpeg)|*.mp3;*.mpg;*.mpeg|All files (*.*)|*.*";
             if (openFileDialog.ShowDialog() == true)
             {
                 chemin = openFileDialog.FileName;
-                nouveauMorceau = new Tracks(textboxTitre.Text, textboxArtiste.Text, textboxAlbum.Text, chemin);
-                listeMorceaux.Add(nouveauMorceau);
-                if (sauvegarde.sauveListe(listeMorceaux) == true)
-                    MessageBox.Show("Les données ont été enregistrées");
+
+                const int SIZE = 128;
+                byte[] data = new byte[SIZE];
+
+                LectureTagMp3 tag = new LectureTagMp3();
+
+                // Read data file .mp3
+
+                string fileName = chemin.Replace("\\\\", "\\");
+
+                using (FileStream file = new FileStream(fileName, FileMode.Open, FileAccess.Read))
+                {
+                    file.Seek(-128, SeekOrigin.End);
+
+                    file.Read(tag.Id, 0, tag.Id.Length);
+                    file.Read(tag.Title, 0, tag.Title.Length);
+                    file.Read(tag.Artist, 0, tag.Artist.Length);
+                    file.Read(tag.Album, 0, tag.Album.Length);
+                    file.Read(tag.Year, 0, tag.Year.Length);
+                    file.Read(tag.Comment, 0, tag.Comment.Length);
+                    file.Read(tag.Genre, 0, tag.Genre.Length);
+                }
+                if (tag.TagId3())
+                {
+                    //MessageBox.Show(tag.ToString());
+                    string contentTexte;
+                    contentTexte = tag.titleToString().Trim(new Char[] { '\r', '\n' });
+                    textboxTitre.Text = contentTexte;
+                    contentTexte = tag.artistToString().Trim(new Char[] { '\r', '\n' });
+                    textboxArtiste.Text = contentTexte;
+                    contentTexte = tag.albumToString().Trim(new Char[] { '\r', '\n' });
+                    textboxAlbum.Text = contentTexte;         
+                }
                 else
-                    MessageBox.Show("Une erreur est survenue, les données n'ont pas été enregistrées");
-
-
+                {
+                    MessageBox.Show("Pas de TAG mp3 dans ce fichier !");
+                    decoupe = chemin.Split('\\');
+                    decoupe = decoupe[decoupe.Length - 1].Split('.');
+                    textboxTitre.Text = decoupe[0];
+                }
             }
         }
 
         private void boutonQuitter_Click(object sender, RoutedEventArgs e)
         {
             this.Close();
+        }
+
+        private void boutonAjouter_Click(object sender, RoutedEventArgs e)
+        {
+            nouveauMorceau = new Tracks(textboxTitre.Text, textboxArtiste.Text, textboxAlbum.Text, chemin);
+            listeMorceaux.Add(nouveauMorceau);
+            if (sauvegarde.sauveListe(listeMorceaux) == true)
+                MessageBox.Show("Les données ont été enregistrées");
+            else
+                MessageBox.Show("Une erreur est survenue, les données n'ont pas été enregistrées");
         }
     }
 }
---
+++ Modeles/LectureTagMp3.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace WpfLecteurAudio.Modeles
+{
+    class LectureTagMp3
+    {
+        private static byte idTagLenght = 3;
+        private static byte titreTagLenght = 30;
+        private static byte artisteTagLenght = 30;
+        private static byte albumTagLenght = 30;
+        private static byte anneeTagLenght = 4;
+        private static byte commentTagLenght = 30;
+        private static byte genreTagLenght = 1;
+
+        public byte[] Id = new byte[idTagLenght];
+        public byte[] Title = new byte[titreTagLenght];
+        public byte[] Artist = new byte[artisteTagLenght];
+        public byte[] Album = new byte[albumTagLenght];
+        public byte[] Year = new byte[anneeTagLenght];
+        public byte[] Comment = new byte[commentTagLenght];
+        public byte[] Genre = new byte[genreTagLenght];
+
+
+        public bool TagId3()
+        {
+            if (Encoding.Default.GetString(Id).Equals("TAG"))
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder tag = new StringBuilder();
+
+            tag.AppendLine(Encoding.Default.GetString(Id));
+            tag.AppendLine(Encoding.Default.GetString(Title));
+            tag.AppendLine(Encoding.Default.GetString(Artist));
+            tag.AppendLine(Encoding.Default.GetString(Album));
+            tag.AppendLine(Encoding.Default.GetString(Year));
+            tag.AppendLine(Encoding.Default.GetString(Comment));
+            tag.AppendLine(Encoding.Default.GetString(Genre));
+
+            return tag.ToString();
+        }
+        public  string titleToString()
+        {
+            StringBuilder tag = new StringBuilder();
+
+            tag.AppendLine(Encoding.Default.GetString(Title));
+
+            return tag.ToString();
+        }
+        public string artistToString()
+        {
+            StringBuilder tag = new StringBuilder();
+
+            tag.AppendLine(Encoding.Default.GetString(Artist));
+
+            return tag.ToString();
+        }
+        public string albumToString()
+        {
+            StringBuilder tag = new StringBuilder();
+
+            tag.AppendLine(Encoding.Default.GetString(Album));
+
+            return tag.ToString();
+        }
+
+    }
+}
